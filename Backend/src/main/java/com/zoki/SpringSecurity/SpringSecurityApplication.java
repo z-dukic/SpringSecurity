07a0906b241ae132dff129c6c18c889eaa387acf
Zoran Đukić <zorandjukic.os@gmail.com>
2022-03-03 19:57:19 +0100

    Creating backend

---
+++ Backend/src/main/java/com/zoki/SpringSecurity/SpringSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.zoki.SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+        System.out.println("Everything is running ok");
+	}
+
+}
