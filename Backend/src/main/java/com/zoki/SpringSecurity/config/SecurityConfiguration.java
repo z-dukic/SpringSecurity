614cc491f16d1417ab88877803d6112ce6b6663b
Zoran Đukić <zorandjukic.os@gmail.com>
2022-03-07 23:38:28 +0100

    Exception for trying to enter without permission

--- Backend/src/main/java/com/zoki/SpringSecurity/config/SecurityConfiguration.java
+++ Backend/src/main/java/com/zoki/SpringSecurity/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -18,10 +19,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserService userService;
 
+    @Autowired
+    private JWTTokenHelper jWTTokenHelper;
+
+    @Autowired
+    private AuthenticationEntryPoint authenticationEntryPoint;
+
 
     //Removes login on start of the application
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
 
         //Allow all that goes through here
         //http.authorizeRequests().anyRequest().permitAll();
