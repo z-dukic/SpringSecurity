b32408d9be5fe07b6b20bec1e900a0165b8c1914
Zoran Đukić <zorandjukic.os@gmail.com>
2022-03-08 22:52:36 +0100

    Creating rest

--- Backend/src/main/java/com/zoki/SpringSecurity/controller/AuthenticationController.java
+++ Backend/src/main/java/com/zoki/SpringSecurity/controller/AuthenticationController.java
@@ -7,6 +7,9 @@ import java.security.spec.InvalidKeySpecException;
 
 import com.zoki.SpringSecurity.config.JWTTokenHelper;
 import com.zoki.SpringSecurity.entity.User;
+import com.zoki.SpringSecurity.requests.AuthenticationRequest;
+import com.zoki.SpringSecurity.response.LoginResponse;
+import com.zoki.SpringSecurity.response.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -36,5 +39,38 @@ public class AuthenticationController {
     private UserDetailsService userDetailsService;
 
 
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> login(@RequestBody AuthenticationRequest authenticationRequest) throws InvalidKeySpecException, NoSuchAlgorithmException {
+
+        final Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+            authenticationRequest.getUserName(), authenticationRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        User user=(User)authentication.getPrincipal();
+        String jwtToken=jWTTokenHelper.generateToken(user.getUsername());
+
+        LoginResponse response=new LoginResponse();
+        response.setToken(jwtToken);
+
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/auth/userinfo")
+    public ResponseEntity<?> getUserInfo(Principal user){
+        User userObj=(User) userDetailsService.loadUserByUsername(user.getName());
+
+        UserInfo userInfo=new UserInfo();
+        userInfo.setFirstName(userObj.getFirstName());
+        userInfo.setLastName(userObj.getLastName());
+        userInfo.setRoles(userObj.getAuthorities().toArray());
+
+
+        return ResponseEntity.ok(userInfo);
+
+
+
+    }
 
 }
